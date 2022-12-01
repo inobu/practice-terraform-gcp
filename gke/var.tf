28cac4b1ae3f5272b1c7c316762f6bea732e0878
inobu <inohkunobuya@gmail.com>
2022-12-01 20:00:44 +0900

    refactor: isolate gke

---
+++ gke/var.tf
@@ -0,0 +1,35 @@
+variable "cluster_name" {
+  default = "cluster"
+}
+
+variable "location" {
+  default = "asia-northeast1-b"
+}
+
+variable "region" {
+  default = "asia-northeast1"
+}
+
+variable "network" {
+  default = "default"
+}
+
+variable "primary_node_count" {
+  default = "3"
+}
+
+variable "machine_type" {
+  default = "e2-medium"
+}
+
+variable "min_master_version" {
+  default = "1.21.9-gke.1002"
+}
+
+variable "node_version" {
+  default = "1.21.9-gke.1002"
+}
+
+variable "preemptible" {
+  default = true
+}
